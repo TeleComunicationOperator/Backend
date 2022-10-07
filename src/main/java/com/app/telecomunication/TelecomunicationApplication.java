5311ca52bd2a85ea5234aff13e5c835e7653a3de
Moises Molina <w7moises@gmail.com>
2022-10-06 21:43:45 -0500

    template added

---
+++ src/main/java/com/app/telecomunication/TelecomunicationApplication.java
@@ -0,0 +1,13 @@
+package com.app.telecomunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomunicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/telecomunication/TelecomunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.telecomunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
